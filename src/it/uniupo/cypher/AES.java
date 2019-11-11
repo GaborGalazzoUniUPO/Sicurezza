d21e8ec649dd58f5f1324c96a9adfd80837e0c03
20024195 <20024195@studenti.uniupo.it>
2019-11-11 09:44:31 +0100

    ok AES

--- src/it/uniupo/cypher/AES.java
+++ src/it/uniupo/cypher/AES.java
@@ -1,10 +1,11 @@
 package it.uniupo.cypher;
 
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.*;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 
@@ -20,22 +21,16 @@ public class AES {
         return  new SecretKeySpec(key, "AES");
     }
 
-    public static void aesCtr(String inFilePath, String outFilePath, boolean mode, SecretKey key) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, BadPaddingException, ShortBufferException, IllegalBlockSizeException {
+    public static byte[] aesCtr(String inFilePath, String outFilePath, boolean mode, SecretKey key, byte[] iv) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, IOException, BadPaddingException, ShortBufferException, IllegalBlockSizeException, InvalidAlgorithmParameterException {
         Cipher cipher = Cipher.getInstance("AES/CTR/NoPadding");
-        cipher.init(mode?Cipher.ENCRYPT_MODE:Cipher.DECRYPT_MODE, key);
+        if(mode) {
+            cipher.init(Cipher.ENCRYPT_MODE, key);
+        }else{
+            cipher.init( Cipher.DECRYPT_MODE, key, new IvParameterSpec(iv));
+        }
 
-        RandomAccessFile inFile = new RandomAccessFile(
-                inFilePath,"r");
-        FileChannel inChannel = inFile.getChannel();
-        ByteBuffer inBuffer = ByteBuffer.allocate((int) inChannel.size());
-        inChannel.read(inBuffer);
 
-        RandomAccessFile outFile = new RandomAccessFile(
-                outFilePath,"rw");
-        FileChannel outChannel = outFile.getChannel();
-        ByteBuffer outBuffer = ByteBuffer.allocate((int) outChannel.size());
-        outChannel.write(outBuffer);
-
-        cipher.doFinal(inBuffer, outBuffer);
+        Files.write(new File(outFilePath).toPath(),cipher.doFinal(Files.readAllBytes(new File(inFilePath).toPath())));
+        return cipher.getIV();
     }
 }
--- src/it/uniupo/cypher/AESTest.java
+++ src/it/uniupo/cypher/AESTest.java
@@ -5,8 +5,8 @@ import org.junit.Test;
 import javax.crypto.*;
 import java.io.File;
 import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
-import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 
 import static org.junit.Assert.*;
@@ -14,9 +14,11 @@ import static org.junit.Assert.*;
 public class AESTest {
 
     @Test
-    public void aesCtr() throws NoSuchPaddingException, ShortBufferException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException {
+    public void aesCtr() throws NoSuchPaddingException, ShortBufferException, NoSuchAlgorithmException, IOException, BadPaddingException, IllegalBlockSizeException, InvalidKeyException, InvalidAlgorithmParameterException {
         SecretKey key = AES.generateKey();
-        AES.aesCtr("/home/20024195/workspace/sicurezza/res/test/aes/input.txt", "/home/20024195/workspace/sicurezza/res/test/aes/out.txt", true, key);
+        byte[] iv = AES.aesCtr("/home/20024195/workspace/sicurezza/res/test/aes/input.txt", "/home/20024195/workspace/sicurezza/res/test/aes/out.txt", true, key, null);
         assertTrue(new File("/home/20024195/workspace/sicurezza/res/test/aes/out.txt").exists());
+        AES.aesCtr("/home/20024195/workspace/sicurezza/res/test/aes/out.txt", "/home/20024195/workspace/sicurezza/res/test/aes/scif.txt",false, key, iv);
+        assertTrue(new File("/home/20024195/workspace/sicurezza/res/test/aes/scif.txt").exists());
     }
 }
